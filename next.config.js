d9954cc6d9c5922769c52a815b98a92a82ecba26
saruulmaa <Saruulmaa1234@gmail.com>
2022-05-23 15:19:54 -0700

    trying

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    PUBLIC_URL: "https://saruul-c.github.io",
+    assetPrefix: './'
+  }
+};
